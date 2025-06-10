709ad064dfff9a0909e23d9624c91918b939e6bf
SsangSoo <kss10080@naver.com>
2025-06-11 06:48:13 +0900

    section 05 ing...

---
+++ src/lambda/lambda5/mystream/MyStreamLoopMain.java
@@ -0,0 +1,16 @@
+package lambda.lambda5.mystream;
+
+import java.util.List;
+
+public class MyStreamLoopMain {
+    public static void main(String[] args) {
+        List<Student> students = List.of(
+                new Student("Apple", 100),
+                new Student("Banana", 80),
+                new Student("Berry", 50),
+                new Student("Tomato", 40)
+        );
+
+
+    }
+}
